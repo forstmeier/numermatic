49012d13e0f763b01f189e6f96a1a0fbc9eb36ad
forstmeier <john.forstmeier@gmail.com>
2022-01-01 12:39:23 -0500

    Build initial data Lambda handler

---
+++ cmd/lambda/data/main.py
@@ -0,0 +1,34 @@
+import numerapi
+import boto3
+
+
+tournament_file_name = 'numerai_tournament_data.parquet'
+
+def handler(event, context):
+    api = numerapi.NumerAPI(
+        public_id=config_data[os.getenv('PUBLIC_ID')],
+        secret_key=config_data[os.getenv('SECRET_KEY')],
+    )
+
+    if not api.check_new_round():
+        return None
+
+    try:
+        api.download_dataset(
+            filename=tournament_file_name,
+            dest_path=filepath,
+        )
+    except Exception as e:
+        return e
+
+    s3 = boto3.client('s3')
+    try:
+        upload_file_response = s3.upload_file(
+            tournament_file_name,
+            os.getenv('DATA_BUCKET_NAME'),
+            'data/'+tournament_file_name,
+        )
+    except Exception as e:
+        return e
+
+    return None

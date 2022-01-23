3e0e36081073ecc43c826d6cb118b9c3145c8b2f
forstmeier <john.forstmeier@gmail.com>
2022-01-23 08:08:44 -0500

    Add notes for "error email" to starter Lambda

--- cmd/lambda/starter/main.py
+++ cmd/lambda/starter/main.py
@@ -1,3 +1,4 @@
+import os
 import zipfile
 import boto3
 
@@ -5,7 +6,7 @@ import boto3
 def handler(event, context):
 	print('event:', event)
 
-    uploads_bucket = event['Records'][0]['s3']['bucket']['name']
+	uploads_bucket = event['Records'][0]['s3']['bucket']['name']
 	key = event['Records'][0]['s3']['object']['key']
 
 	key_split = key.split('/')
@@ -25,7 +26,7 @@ def handler(event, context):
 		)
 
 		zip_ref = zipfile.ZipFile(local_file)
-		zip_ref.extractall(dir_name)
+		zip_ref.extractall('/tmp/')
 		zip_ref.close()
 
 		models_bucket = os.getenv('MODELS_BUCKET_NAME')
@@ -42,37 +43,13 @@ def handler(event, context):
 			Key='{}/{}/models/requirements.txt'.format(user_id, execution_id),
 		)
 
+		return None # TEMP
+
 	except Exception as e:
 		print('exception:', e)
 
-		get_item_response = dynamodb.get_item(
-			TableName=os.getenv('USERS_TABLE_NAME'),
-			Key={
-				'id': {
-					'S': event['headers']['numermatic-user-id'],
-				},
-			},
-		)
-		print('get_item_response:', get_item_response)
-
-		email = get_item_response['Item']['email']['S']
-
-		ses = boto3.client('ses')
-
-        send_email_response = ses.send_email(
-            Source=os.getenv("SOURCE_EMAIL"),
-            Destination=email,
-            Message={
-                Subject: {
-                    Data: 'Your model has encountered an error!',
-                },
-                Body: {
-                    Text: {
-                        Data: 'Contact the project owner for more information. Execution ID: {}'.format(execution_id),
-                    }
-                }
-            },
-        )
-		print('send_email_response:', send_email_response)
-
-	return None
+		# outline:
+		# [ ] get user email
+		# [ ] send error email
+		# - [ ] possibly trigger "failure" lambda
+		# - [ ] send email

abc964a694bf8b259c4a984e69b1b6b7b6c6ed6a
forstmeier <john.forstmeier@gmail.com>
2022-01-26 18:30:23 -0500

    Remove Stripe resources from users Lambda

--- cmd/lambda/users/main.py
+++ cmd/lambda/users/main.py
@@ -3,7 +3,6 @@ import os
 import time
 import uuid
 import boto3
-import stripe
 
 
 def handler(event, context):
@@ -11,32 +10,11 @@ def handler(event, context):
 
     user_id = str(uuid.uuid4().hex)
 
-    stripe.api_key = os.getenv('STRIPE_API_KEY')
-
     body = event['body']
 
     dynamodb = boto3.client('dynamodb')
 
     try:
-        create_payment_method_response = stripe.PaymentMethod.create(
-            type="card",
-            card={
-                "number": body['number'],
-                "exp_month": body['exp_month'],
-                "exp_year": body['exp_year'],
-                "cvc": body['cvc'],
-            },
-        )
-
-        create_customer_response = stripe.Customer.create(
-            email=body['email'],
-        )
-
-        attach_method_response = stripe.PaymentMethod.attach(
-            create_payment_method_response['id'],
-            customer=create_customer_response['id'],
-        )
-
         put_item_response = dynamodb.put_item(
             TableName=os.getenv('USERS_TABLE_NAME'),
             Item={
@@ -45,9 +23,6 @@ def handler(event, context):
                 },
                 'email': {
                     'S': body['email'],
-                },
-                'stripe_customer_id': {
-                    'S': create_customer_response['id'],
                 },
 				'timestamp': {
 					'S': time.time(),
